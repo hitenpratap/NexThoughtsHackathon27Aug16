6ad4395a3a0e13a3e753b4a4ef7d8d369da5f03a
Anuj Nigam <anujnigam.friends@gmail.com>
2016-08-27 12:52:14 +0530

    admin added

--- grails-app/controllers/com/hackathon/RssFeedController.groovy
+++ grails-app/controllers/com/hackathon/RssFeedController.groovy
@@ -12,16 +12,20 @@ class RssFeedController {
 
     def fetchRssContent(){
         URL feedUrl = new URL("http://www.banktech.com/rss_simple.asp?f_n=949&f_ln=Banking%20Channels");
-
-        SyndFeedInput input = new SyndFeedInput();
-        SyndFeed feed = input.build(new XmlReader(feedUrl));
-        for(int i=0;i<feed.entries.size();i++){
-            if(feed.entries[i].description.value.find(/(paytm)|(payments)|(fintech)|(freecharge)|(faircent)|(lending kart)/)){
-                println feed.entries[i].title
-                println feed.entries[i].link
-                println feed.entries[i].description.value
+        try {
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(feedUrl));
+            for(int i=0;i<feed.entries.size();i++){
+                if(feed.entries[i].description.value.find(/(paytm)|(payments)|(fintech)|(freecharge)|(faircent)|(lending kart)/)){
+                    println feed.entries[i].title
+                    println feed.entries[i].link
+                    println feed.entries[i].description.value
+                }
             }
+        }catch (Exception e){
+            println("Exception is "+e.getMessage())
         }
+
         render  ""+feed
     }
 }
