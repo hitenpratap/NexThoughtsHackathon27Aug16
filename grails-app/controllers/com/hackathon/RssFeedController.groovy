bacdf0d93c3cedd50a827979d739becfadf4dab2
shipra tayal <shipra@nexthoughts.com>
2016-08-27 12:54:44 +0530

    code pull

--- grails-app/controllers/com/hackathon/RssFeedController.groovy
+++ grails-app/controllers/com/hackathon/RssFeedController.groovy
@@ -1,5 +1,6 @@
 package com.hackathon
 
+import com.fintechtimes.Article.ArticleQueueManager
 import com.rometools.rome.feed.synd.SyndFeed
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader
@@ -15,11 +16,17 @@ class RssFeedController {
 
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(feedUrl));
+        ArticleQueueManager articleQueueManager=new ArticleQueueManager()
         for(int i=0;i<feed.entries.size();i++){
             if(feed.entries[i].description.value.find(/(paytm)|(payments)|(fintech)|(freecharge)|(faircent)|(lending kart)/)){
                 println feed.entries[i].title
                 println feed.entries[i].link
                 println feed.entries[i].description.value
+                if(!ArticleQueueManager.findByArticleURL(feed.entries[i].link))
+                articleQueueManager.articleURL=feed.entries[i].link
+
+                articleQueueManager.save()
+
             }
         }
         render  ""+feed
